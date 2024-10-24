12b3604328717a8782f13820702e0e8f633c7663
ferryzhou <ferryzhou@gmail.com>
2024-10-23 19:35:28 -0700

    Create joystick_radio.js

---
+++ joystick_radio.js
@@ -0,0 +1,31 @@
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
+    joystickbit.Vibration_Motor(100)
+    basic.showString("E")
+    radio.sendValue("z", 0)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
+    joystickbit.Vibration_Motor(100)
+    basic.showString("F")
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
+    joystickbit.Vibration_Motor(100)
+    basic.showString("D")
+    radio.sendValue("z", 100)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
+    joystickbit.Vibration_Motor(100)
+    basic.showString("C")
+})
+joystickbit.initJoystickBit()
+music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
+
+let y = 0
+let x = 0
+joystickbit.initJoystickBit()
+radio.setGroup(1)
+basic.forever(function () {
+    x = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 1023, 0, -100, 100)
+    y = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.Y), 1023, 0, 100, -100)
+    radio.sendValue("x", x)
+    radio.sendValue("y", y)
+})
